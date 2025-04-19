f732e81b1b65de7a31336cbf21d08204119a6c05
MohdShahulMalik <mohdshahulmalik@gmail.com>
2025-04-19 20:48:57 +0530

    Fixed tsconfig so that it works well with CommonJS modules and updated the backend endpoint to not just extract an article but to extract multiple articles and summarize them as well

--- news-app-backend/src/index.ts
+++ news-app-backend/src/index.ts
@@ -1,32 +1,38 @@
 import express, { Express, Request, Response } from "express";
-import { extract, Article } from "@extractus/article-extractor";
+import { ArticleData, extract } from "@extractus/article-extractor";
 import { load } from "cheerio";
 
 const app: Express = express();
 const PORT: number = 8080;
+app.use(express.json());
 
-type SrcUrl = {
-    url: string;
-}
+type ArticleUrls = {
+    urls: string[];
+} 
 
-app.get("/extract", async (req: Request<{}, {}, {}, SrcUrl>, res: Response) => {
-    const { url } = req.query;
+app.post("/summarize", async (req: Request<null, null, ArticleUrls, null>, res: Response) => {
+    const { urls } = req.body;
 
-    if (!url) {
-        res.status(400).json({ error: "Missing 'url' query parameter" });
+    if (!urls || !Array.isArray(urls) || urls.length === 0) {
+        res.status(400).json({ error: "Invalid body" });
         return;
     }
 
+    const articlesContent: string[] = [];
     try {
-        const article: Article | null = await extract(url);
-        
-        if (!article) {
-            res.status(404).json({ error: "Could not extract the article" });
-            return;
+        for (const url of urls) {
+            const article: ArticleData | null = await extract(url);
+            
+            if (!article) {
+                continue;
+            }
+            const $ = await load(article.content ?? "");
+            const content = $.text();
+            if (content){
+                articlesContent.push(content);
+            }
         }
-        const $ = await load(article.content ?? "");
-        const content = $.text();
-        res.json({content});
+        res.json(articlesContent);
     }catch (error) {
         if (error instanceof Error){
             console.error("Failed to extract the article ", error.message);
@@ -36,8 +42,14 @@ app.get("/extract", async (req: Request<{}, {}, {}, SrcUrl>, res: Response) => {
 
         res.status(500).json({ error: "Failed to extract the article " });
     }
+
+    try {
+        //TODO: Add a function to summarize the articlesContent array and return the summary
+    }catch(error) {
+        console.log(error);
+    }
 });
 
 app.listen(PORT, () => {
-    console.log(`Listening at http://localhost:${PORT}/extract`);
+    console.log(`Listening at http://localhost:${PORT}/summarize`);
 });
