182559afcf15a10dace71523349a6bd17a66f265
MohdShahulMalik <mohdshahulmalik@gmail.com>
2025-04-23 00:21:22 +0530

    Completed the summarize endpoint that will extract and summarize the articles

--- news-app-backend/src/index.ts
+++ news-app-backend/src/index.ts
@@ -1,9 +1,11 @@
 import express, { Express, Request, Response } from "express";
 import { ArticleData, extract } from "@extractus/article-extractor";
 import { load } from "cheerio";
+import dotenv from "dotenv";
 
+dotenv.config();
 const app: Express = express();
-const PORT: number = 8080;
+const PORT: number = process.env.PORT ? parseInt(process.env.PORT): 8080;
 app.use(express.json());
 
 type ArticleUrls = {
@@ -19,6 +21,7 @@ app.post("/summarize", async (req: Request<null, null, ArticleUrls, null>, res: 
     }
 
     const articlesContent: string[] = [];
+    const summarizedArticles: string[] = [];
     try {
         for (const url of urls) {
             const article: ArticleData | null = await extract(url);
@@ -32,7 +35,6 @@ app.post("/summarize", async (req: Request<null, null, ArticleUrls, null>, res: 
                 articlesContent.push(content);
             }
         }
-        res.json(articlesContent);
     }catch (error) {
         if (error instanceof Error){
             console.error("Failed to extract the article ", error.message);
@@ -45,8 +47,38 @@ app.post("/summarize", async (req: Request<null, null, ArticleUrls, null>, res: 
 
     try {
         //TODO: Add a function to summarize the articlesContent array and return the summary
+        for (const article of articlesContent){
+            const response = await fetch(`https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash-lite:generateContent?key=${process.env.GOOGLE_API_KEY}`,{
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({
+                    contents: [
+                        {
+                            parts: [
+                                {text: "Just summarize the following news article in few concise bullet points and highlight the main thing at the beggining of the point, give it a concise heading also, your response should contain nothing else: " + article}
+                            ]
+                        }
+                    ]
+                })
+            });
+            
+            const jsonResponse = await response.json();
+            // console.dir(jsonResponse, { depth: null });
+            const summarizedArticle = jsonResponse?.candidates?.[0]?.content?.parts?.[0]?.text;
+            summarizedArticles.push(summarizedArticle);
+        }
+
+        res.json({ summarizedArticles });
     }catch(error) {
-        console.log(error);
+        if (error instanceof Error){
+            console.error("Failed to summarize the article ", error.message);
+        }else {
+            console.error("Failed to summarize the article ", error);
+        }
+
+        res.status(500).json({ error: "Failed to summarize the article " });
     }
 });
 
