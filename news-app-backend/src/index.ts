2fd973b0f723cd7f20d6c136375e3bfaae83b846
MohdShahulMalik <mohdshahulmalik@gmail.com>
2025-04-24 22:42:13 +0530

    Added a new endpoint 'fact-check' and added surrealdb as a dependency

--- news-app-backend/src/index.ts
+++ news-app-backend/src/index.ts
@@ -2,6 +2,7 @@ import express, { Express, Request, Response } from "express";
 import { ArticleData, extract } from "@extractus/article-extractor";
 import { load } from "cheerio";
 import dotenv from "dotenv";
+import { GoogleGenAI } from "@google/genai";
 
 dotenv.config();
 const app: Express = express();
@@ -11,9 +12,26 @@ app.use(express.json());
 
 type ArticleUrls = {
     urls: string[];
-} 
+};
 
-app.post("/summarize", async (req: Request<null, null, ArticleUrls, null>, res: Response) => {
+type SummarizedArticles = {
+    summarizedArticles: string[];
+};
+
+type ErrorResponse = {
+    error: string;
+    
+};
+
+type Claim = {
+    claim: string;
+};
+
+type Assessment = {
+    assement: string;
+};
+
+app.post("/summarize", async (req: Request<null, SummarizedArticles | ErrorResponse, ArticleUrls, null>, res: Response) => {
     const { urls } = req.body;
 
     if (!urls || !Array.isArray(urls) || urls.length === 0) {
@@ -47,7 +65,6 @@ app.post("/summarize", async (req: Request<null, null, ArticleUrls, null>, res: 
     }
 
     try {
-        //TODO: Add a function to summarize the articlesContent array and return the summary
         for (const article of articlesContent){
             const response = await fetch(`https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash-lite:generateContent?key=${process.env.GOOGLE_API_KEY}`,{
                 method: "POST",
@@ -83,6 +100,41 @@ app.post("/summarize", async (req: Request<null, null, ArticleUrls, null>, res: 
     }
 });
 
+app.get("/fact-check", async (req: Request<null, Assessment | ErrorResponse, null, Claim>, res: Response) => {
+    const { claim } = req.query;
+    if (!claim) {
+        res.status(400).json({ error: "Must provide a 'claim' query to fact check"});
+        return;
+    }
+
+    try {
+        const ai = new GoogleGenAI({ apiKey: process.env.GOOGLE_API_KEY2 });
+        const response = await ai.models.generateContent({
+            model: "gemini-2.0-flash",
+            contents: [
+                `assess the truthfulness of this claim(true or flase): "${claim}"`,
+            ],
+            config: {
+                tools: [
+                    {
+                        googleSearch: {}
+                    }
+                ],
+            },
+        });
+    
+        res.json({ assessment: response.text });
+    }catch(error) {
+        if (error instanceof Error){
+            console.error("Failed to fact-check the claim,", error.message);
+        }else {
+            console.error("Failed to fact-check the claim,", error);
+        }
+
+        res.status(500).json({ error: "Failed to fact-check the claim" });
+    }
+});
+
 app.listen(PORT, () => {
-    console.log(`Listening at http://localhost:${PORT}/summarize`);
+    console.log(`Listening at http://localhost:${PORT}`);
 });
